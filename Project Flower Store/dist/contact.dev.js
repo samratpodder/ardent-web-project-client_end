27f75176fdcc205807662d0e4360cc15a28983d2
samratpodder <iamsamrat16@outlook.com>
2020-09-04 12:25:53 +0530

    contact page finished without navbar fully responsive

---
+++ Project Flower Store/dist/contact.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var sendKey = document.getElementById("send");
+sendKey.addEventListener("mousemove", function () {
+  sendKey.classList.remove("btn-outline-danger");
+  sendKey.classList.add("btn-outline-success");
+});
+sendKey.addEventListener("mouseout", function () {
+  sendKey.classList.add("btn-outline-danger");
+  sendKey.classList.remove("btn-outline-success");
+});
