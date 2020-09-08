9f73d468291877ff55b145b68ed329309bf79b2a
samratpodder <iamsamrat16@outlook.com>
2020-09-08 12:18:15 +0530

    Completed basic HTML of product page with minimal styling without navbar

---
+++ Project Flower Store/product.js
@@ -0,0 +1,43 @@
+var stars = document.getElementsByClassName("fa-star");
+var rating = 3;
+
+for(var i =0;i<rating;i++)
+{
+    stars[i].classList.remove("far");
+    stars[i].classList.add("fas");
+}
+var getnumreviews = 12
+var numreview = document.getElementById("numreview");
+numreview.innerText = getnumreviews;
+var getprice = 5.45
+var price = document.getElementById("prod-price");
+price.innerText = getprice;
+var plusbut = document.querySelector(".qtyplus");
+var qtydisplay = document.querySelector(".boxed");
+var qtyval = Number(qtydisplay.innerText);
+console.log(qtyval);
+plusbut.addEventListener("click", function (){
+    qtyval+=1;
+    qtydisplay.innerText = qtyval;
+    console.log(qtyval);
+});
+var minusbut = document.querySelector(".qtyminus");
+minusbut.addEventListener("click", function (){
+    qtyval-=1;
+    console.log(qtyval);
+    if(qtyval<=0)
+    {
+       qtyval=0;
+    }
+    qtydisplay.innerText = qtyval;
+});
+var colorbutton = document.querySelectorAll(".colorbutton");
+colorbutton[0].addEventListener("click", function(){
+    colorbutton[0].style.border = "2px solid red";
+});
+colorbutton[1].addEventListener("click", function(){
+    console.log(colorbutton[1]);
+});
+colorbutton[2].addEventListener("click", function(){
+    console.log(colorbutton[2]);
+});
