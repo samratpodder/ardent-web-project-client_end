f4fafaf66497321e8203a5cbb30ccfc3e9e22fba
samratpodder <iamsamrat16@outlook.com>
2020-09-13 17:42:01 +0530

    complete home page without styling and navbars

--- Project Flower Store/dist/home.dev.js
+++ Project Flower Store/dist/home.dev.js
@@ -5,3 +5,24 @@ function Scrolldown() {
 }
 
 window.onload = Scrolldown();
+var reviewPictures = document.querySelectorAll(".review-picture");
+var reviewblock = document.querySelectorAll(".review-text");
+var i = 0;
+setInterval(showReview, 2000);
+
+function showReview() {
+  reviewblock[i].classList.toggle("d-none");
+  reviewPictures[i].style.opacity = 1;
+
+  for (var j = 0; j < 3; j++) {
+    if (j !== i) {
+      reviewPictures[j].style.opacity = 0.6;
+      reviewblock[j].classList.add("d-none");
+    }
+  } // setTimeout(nowork,1500);
+  // reviewblock[i].classList.toggle("d-none");
+
+
+  i++;
+  i = i % 3;
+}
