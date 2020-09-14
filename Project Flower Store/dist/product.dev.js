8823eaaa4f6f4daa605fbfca2a35950e3bf312bd
samratpodder <iamsamrat16@outlook.com>
2020-09-14 12:08:32 +0530

    #Probable Final Commit. Project Finished with almost done styling with main content and Navbar.

--- Project Flower Store/dist/product.dev.js
+++ Project Flower Store/dist/product.dev.js
@@ -44,3 +44,24 @@ colorbutton[1].addEventListener("click", function () {
 colorbutton[2].addEventListener("click", function () {
   console.log(colorbutton[2]);
 });
+var reviewPictures = document.querySelectorAll(".review-picture");
+var reviewblock = document.querySelectorAll(".review-text");
+var i = 0;
+setInterval(showReview, 2000);
+
+function showReview() {
+  reviewblock[i].classList.toggle("d-none");
+  reviewPictures[i].style.opacity = 1;
+
+  for (var j = 0; j < 3; j++) {
+    if (j !== i) {
+      reviewPictures[j].style.opacity = 0.6;
+      reviewblock[j].classList.add("d-none");
+    }
+  } // setTimeout(nowork,1500);
+  // reviewblock[i].classList.toggle("d-none");
+
+
+  i++;
+  i = i % 3;
+}
